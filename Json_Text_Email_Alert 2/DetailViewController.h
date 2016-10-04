659c80c990aaa3f2795a685a40e01d0fd93fca7a
dhavni1234 <dhavni1234@rediffmail.com>
2016-10-04 10:26:49 +0530

    Add files via upload

---
+++ Json_Text_Email_Alert 2/DetailViewController.h
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.h
+//  Json
+//
+//  Created by Raghu Bansal on 9/27/16.
+//  Copyright © 2016 RWS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+
+@interface DetailViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *lblEmail;
+@property (weak, nonatomic) IBOutlet UILabel *lblFullName;
+@property (weak, nonatomic) IBOutlet UILabel *lblTitle;
+
+@property NSDictionary *data1;
+@end
