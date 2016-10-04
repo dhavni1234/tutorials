329dedc162fb44b10a6fbe156d99c17842931636
dhavni1234 <dhavni1234@rediffmail.com>
2016-10-04 10:24:43 +0530

    Add files via upload

---
+++ Json_Text_Email_Alert_Emoji/Json/ViewController.h
@@ -0,0 +1,22 @@
+//
+//  ViewController.h
+//  Json
+//
+//  Created by Matt on 6/21/14.
+//  Copyright (c) 2014 RWS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DetailViewController.h"
+
+@interface ViewController : UIViewController <UITextFieldDelegate>
+
+- (IBAction)showJson:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UITextField *txtFieldEmail;
+@property (weak, nonatomic) IBOutlet UITextField *txtFieldPassword;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (weak, nonatomic) IBOutlet UIButton *btnOutletJson;
+
+
+@end
