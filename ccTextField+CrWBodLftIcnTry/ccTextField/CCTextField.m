6d42ba12c3d57cddce68823d528340629b060d66
dhavni1234 <dhavni1234@rediffmail.com>
2016-10-25 09:13:51 +0530

    Add files via upload

---
+++ ccTextField+CrWBodLftIcnTry/ccTextField/CCTextField.m
@@ -0,0 +1,88 @@
+//
+//  CCTextField.m
+//  ccTextField
+//
+//  Created by Raghu Bansal on 10/24/16.
+//  Copyright © 2016 XtreemSolution. All rights reserved.
+//
+
+#import "CCTextField.h"
+
+@implementation CCTextField
+{
+    UIImageView *leftIconView;
+    UIView *leftContainerView;
+    UIImageView *leftIconImageView;
+}
+
+- (void) awakeFromNib
+{
+    [super awakeFromNib];
+    [self initialize];
+}
+
+
+
+- (id)initWithFrame:(CGRect)frame {
+    if ((self = [super initWithFrame:frame])) {
+        [self initialize];
+        
+    }
+    return self;
+}
+
+
+// Shows the proper changes in story board.
+- (void)prepareForInterfaceBuilder{
+    [self initialize];
+}
+
+
+
+-(void)initialize {
+    self.tintColor = [UIColor greenColor]; // code for curser color when we enter text to text field.
+    [self initializeView];
+    
+    if(self.leftIcon)
+    {
+        [self addLeftIcon];
+        
+    }
+
+}
+-(void)addLeftIcon
+{
+    leftContainerView = [[UIView alloc] init];
+
+    leftIconImageView = [[UIImageView alloc] initWithImage:self.leftIcon];
+    [leftIconImageView setBackgroundColor:[UIColor clearColor]];
+    [leftContainerView addSubview:leftIconImageView];
+    [self setLeftView:leftContainerView];
+    [self setLeftViewMode:UITextFieldViewModeAlways];
+}
+
+-(void)drawRect:(CGRect)rect
+{
+    
+    [leftContainerView setFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height+2)];
+    [leftIconImageView setFrame:CGRectMake(self.frame.size.height/6, (self.frame.size.height/7), 21, 22)]; 
+    leftContainerView.frame  = CGRectMake(0, 0, self.frame.size.height, self.frame.size.height) ;
+}
+
+
+
+
+- (void)initializeView {
+    if(self.borderColor == nil)
+    {
+        self.borderColor = [UIColor whiteColor];
+    }
+    
+    self.layer.cornerRadius = self.cornerRadius;
+    self.layer.borderColor = self.borderColor.CGColor;
+    self.layer.borderWidth = self.borderWidth;
+}
+
+
+@end
+
---
+++ ccTextField+CrWBodLftIcnTry/ccTextField/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ccTextField
+//
+//  Created by Raghu Bansal on 10/24/16.
+//  Copyright © 2016 XtreemSolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
