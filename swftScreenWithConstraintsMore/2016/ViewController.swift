9f228e890afff4f51c7a0843a2c281163bff0e93
dhavni1234 <dhavni1234@rediffmail.com>
2016-11-16 09:06:15 +0530

    Add files via upload

---
+++ swftScreenWithConstraintsMore/2016/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  2016
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    @IBOutlet var txtFieldCollection: [UITextField]!
+    @IBOutlet weak var txtStartingDate: UITextField!
+    @IBOutlet weak var txtFrequency: UITextField!
+    @IBOutlet weak var txtEstimatedFare: UITextField!
+    @IBOutlet weak var txtDistance: UITextField!
+    @IBOutlet weak var txtFrom: UITextField!
+    @IBOutlet weak var txtTo: UITextField!
+    @IBOutlet weak var txtExclusiveRide: UITextField!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // scrolling code.
+      //  scrollView.contentInset = UIEdgeInsetsMake(0, 0, 700, 0)
+
+        //txtFieldsCollection
+        for txtFields in txtFieldCollection {
+            txtFields.isEnabled = false
+        }
+
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
