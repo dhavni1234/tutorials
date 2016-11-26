8975bc9f286893eae78355121522b1e0fd0b624e
dhavni1234 <dhavni1234@rediffmail.com>
2016-11-26 09:24:19 +0530

    Add files via upload

---
+++ Task2.TableViewBTnDataTrfScrNw2/TableViewController.h
@@ -0,0 +1,16 @@
+//
+//  TableViewController.h
+//  TaskTableViewBTnDataTrf
+//
+//  
+//
+
+#import <UIKit/UIKit.h>
+#import "TableViewCell.h"
+#import "UserInfo1Controller.h"
+
+@interface TableViewController : UITableViewController<UITableViewDelegate,UITableViewDataSource>
+
+
+
+@end
