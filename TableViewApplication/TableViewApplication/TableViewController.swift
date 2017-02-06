4721dd95a4f3ecdaa626fcacce51af75163acfeb
dhavni1234 <dhavni1234@rediffmail.com>
2017-02-07 00:19:31 +0530

    Add files via upload

---
+++ TableViewApplication/TableViewApplication/TableViewController.swift
@@ -0,0 +1,153 @@
+//
+//  TableViewController.swift
+//  TableViewApplication
+//
+//  Created by Aaron on 03/08/2015.
+//  Copyright © 2015 Aaron. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    
+    let ImageList = ["burj.jpg","Eiffel.jpg","empire.jpg","kingdomtower.jpg","Taipei.jpg"]
+    
+    let TitleList = ["Burj Khalifa","Eiffel Tower","Empire State Building","Kingdom Tower","Taipei 101"]
+
+    let DescriptionList = ["Dubai, UAE","Paris, France","New York, USA","Jeddah, Saudi Arabia","Taipei, Taiwan"]
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return TitleList.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        let cell: TableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell") as! TableViewCell
+        
+        
+        cell.CellTitle.text = TitleList[indexPath.row]
+        cell.CellDescription.text = DescriptionList[indexPath.row]
+        
+        let imagename = UIImage(named: ImageList[indexPath.row])
+        cell.CellImage.image = imagename
+        
+        return cell
+        
+        
+        
+        
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if (segue.identifier == "DetailView") {
+            
+            let VC = segue.destinationViewController as! DetailViewController
+            if let indexpath = self.tableView.indexPathForSelectedRow {
+                
+                let Title = TitleList[indexpath.row] as String
+                VC.SentData1 = Title
+                
+                let Description = DescriptionList[indexpath.row] as String
+                VC.SentData2 = Description
+                
+                let Imageview = ImageList[indexpath.row] as String
+                VC.SentData3 = Imageview
+                
+            }
+            
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
